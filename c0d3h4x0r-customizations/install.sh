797c7e626edfbdef4820ba2d016540a8f7c83b52
Keith F. Kelly <keith.f.kelly@outlook.com>
2020-11-30 19:48:28 -0800

    improve install script

--- c0d3h4x0r-customizations/install.sh
+++ c0d3h4x0r-customizations/install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 
-cp -p -r ./boot.$1/* /boot/
-cp -p -r ./etc/* /etc/
-cp -p -r ./home/* /home/
-cp -p -r ./opt/* /opt/
+if [ ! -d "./boot.$1" ]; then
+    echo "USAGE: $0 <rpi3bplus|rpi4>"
+    exit 1
+fi
+
+COPY_OPTIONS='-b --no-preserve=ownership -r --suffix=.bak'
+
+sudo cp $COPY_OPTIONS "./boot.$1"/* /boot/
+sudo cp $COPY_OPTIONS ./etc/* /etc/
+cp $COPY_OPTIONS ./home/pi/* /home/pi/
+sudo cp $COPY_OPTIONS ./opt/* /opt/
