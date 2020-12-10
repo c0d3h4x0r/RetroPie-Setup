3a681806668388b3c448589d3a9f09d8799bcfb7
Keith F. Kelly <keith.f.kelly@outlook.com>
2020-12-09 20:23:43 -0800

    fix cmdline.txt and config.txt stuff

--- c0d3h4x0r-customizations/install.sh
+++ c0d3h4x0r-customizations/install.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env sh
 
-if [ ! -d "./boot.$1" ]; then
-    echo "USAGE: $0 <rpi3bplus|rpi4>"
-    exit 1
-fi
-
 COPY_OPTIONS='-b --no-preserve=ownership -r --suffix=.bak'
 
-sudo cp $COPY_OPTIONS "./boot.$1"/* /boot/
+sudo cp $COPY_OPTIONS ./boot/* /boot/
 sudo cp $COPY_OPTIONS ./etc/* /etc/
 cp $COPY_OPTIONS ./home/pi/* /home/pi/
 sudo cp $COPY_OPTIONS ./opt/* /opt/
 sudo chown -R pi:pi /opt/retropie/configs
 sudo apt-get install -y mpg123 bc
+
+echo "Now go modify /boot/config.txt to add/modify the parameters found in /boot/config.txt.silent-boot"
