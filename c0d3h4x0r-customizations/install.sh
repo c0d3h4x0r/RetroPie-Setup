1087b232d8fb3e47dc345c51b85a39b18a164777
Keith F. Kelly <keith.f.kelly@outlook.com>
2020-12-11 13:57:11 -0800

    Fix permissions and mode on bgm-* scripts

--- c0d3h4x0r-customizations/install.sh
+++ c0d3h4x0r-customizations/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 COPY_OPTIONS='-b --no-preserve=ownership -r --suffix=.bak'
 
@@ -7,6 +7,11 @@ sudo cp $COPY_OPTIONS ./etc/* /etc/
 cp $COPY_OPTIONS ./home/pi/* /home/pi/
 sudo cp $COPY_OPTIONS ./opt/* /opt/
 sudo chown -R pi:pi /opt/retropie/configs
-sudo apt-get install -y mpg123 bc
+pushd /opt/retropie/supplementary/emulationstation/scripts > /dev/null
+sudo chmod 4751 ./bgm-player ./bgm-start ./bgm-stop
+sudo chgrp pi ./bgm-player ./bgm-start ./bgm-stop
+popd > /dev/null
+sudo apt-get install -y	mpg123  # for bgm-player
+sudo apt-get install -y bc  # for hw_status
 
-echo "Now go modify /boot/config.txt to add/modify the parameters found in /boot/config.txt.silent-boot"
+echo "Now go modify /boot/cmdline.txt to add/modify the parameters found in /boot/cmdline.txt.silent-boot"
