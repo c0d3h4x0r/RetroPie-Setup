ca99fc99bfdc06a177f831fbf5573f835a788c8f
Keith F. Kelly <keith.f.kelly@outlook.com>
2020-12-16 17:22:01 -0800

    fix perms on bgm-* scripts

--- c0d3h4x0r-customizations/install.sh
+++ c0d3h4x0r-customizations/install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-BACKUP_SUFFIX=.bak
-COPY_OPTIONS="-b --no-preserve=ownership -r --suffix=$BACKUP_SUFFIX"
+COPY_OPTIONS="--no-preserve=ownership -r"
 
 sudo cp $COPY_OPTIONS ./boot/* /boot/
 sudo cp $COPY_OPTIONS ./etc/* /etc/
@@ -9,8 +8,8 @@ cp $COPY_OPTIONS ./home/pi/* /home/pi/
 sudo cp $COPY_OPTIONS ./opt/* /opt/
 sudo chown -R pi:pi /opt/retropie/configs
 pushd /opt/retropie/supplementary/emulationstation/scripts > /dev/null
-sudo chmod 4751 ./bgm-player ./bgm-start ./bgm-stop
 sudo chgrp pi ./bgm-player ./bgm-start ./bgm-stop
+sudo chmod 2654 ./bgm-player ./bgm-start ./bgm-stop
 popd > /dev/null
 sudo apt-get install -y mpg123  # for bgm-player
 sudo apt-get install -y bc  # for hw_status
