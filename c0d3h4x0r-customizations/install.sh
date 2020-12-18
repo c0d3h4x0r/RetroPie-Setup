92beb5841600f6fb0543ca7c4f98ef7fb610f6b7
Keith F. Kelly <keith.f.kelly@outlook.com>
2020-12-17 17:16:14 -0800

    fix install script

--- c0d3h4x0r-customizations/install.sh
+++ c0d3h4x0r-customizations/install.sh
@@ -1,27 +1,34 @@
 #!/usr/bin/env bash
 
+set -e
+
+if [[ "$(id -u)" -ne 0 ]]; then
+    echo "This script must be run using 'sudo'."
+    exit 1
+fi
+
 SCRIPT_DIR="$(dirname "$0")"
 SCRIPT_DIR="$(cd "$SCRIPT_DIR" && pwd)"
 
 COPY_OPTIONS="--no-preserve=ownership -r"
 
-sudo cp $COPY_OPTIONS $SCRIPT_DIR/boot /
-sudo cp $COPY_OPTIONS $SCRIPT_DIR/etc /
-cp $COPY_OPTIONS $SCRIPT_DIR/home /
-sudo cp $COPY_OPTIONS $SCRIPT_DIR/root /
+cp $COPY_OPTIONS $SCRIPT_DIR/boot /
+cp $COPY_OPTIONS $SCRIPT_DIR/etc /
+sudo -u $USER cp $COPY_OPTIONS $SCRIPT_DIR/home/pi /home/
+cp $COPY_OPTIONS $SCRIPT_DIR/root /
 
-sudo cp $COPY_OPTIONS $SCRIPT_DIR/opt /
-sudo chown -R pi:pi /opt/retropie/configs
+cp $COPY_OPTIONS $SCRIPT_DIR/opt /
+chown -R pi:pi /opt/retropie/configs
 
 pushd /opt/retropie/supplementary/emulationstation/scripts > /dev/null
-sudo chgrp pi ./bgm-player ./bgm-start ./bgm-stop
-sudo chmod 2654 ./bgm-player ./bgm-start ./bgm-stop
+chgrp pi ./bgm-player ./bgm-start ./bgm-stop
+chmod 2654 ./bgm-player ./bgm-start ./bgm-stop
 popd > /dev/null
 
-sudo apt-get install -y mpg123  # for bgm-player
-sudo apt-get install -y bc  # for hw_status
+apt-get install -y mpg123  # for bgm-player
+apt-get install -y bc  # for hw_status
 
 # set cmdline for silent boot
-sudo sed --in-place 's/ quiet//g; s/$/ quiet/g' /boot/cmdline.txt
+sed --in-place 's/ quiet//g; s/$/ quiet/g' /boot/cmdline.txt
 
 echo "Remember to run argoneone-install and argonone-config to make fan always run if you are using an Argon ONE case."
