e569d183eed3cb0c202971aa28dbab8357bb337e
Keith F. Kelly <keith.f.kelly@outlook.com>
2021-01-27 20:02:44 -0800

    set cpu governor to always be performance

--- c0d3h4x0r-customizations/install.sh
+++ c0d3h4x0r-customizations/install.sh
@@ -31,4 +31,10 @@ apt-get install -y bc  # for hw_status
 # set cmdline for silent boot
 sed --in-place 's/ quiet//g; s/$/ quiet/g' /boot/cmdline.txt
 
-echo "Remember to run argoneone-install and argonone-config to make fan always run if you are using an Argon ONE case."
+# ensure CPU governor is always set to performance
+sed -z -i -E 's/raspi-config //g; s/raspi-config: [^\n]*\n//g' .depend.boot
+sed -z -i -E 's/raspi-config //g; s/raspi-config: [^\n]*\n//g' .depend.start
+SYS_CPU_GOVERNOR=/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+[ -e "$SYS_CPU_GOVERNOR" ] && echo "performance" > "$SYS_CPU_GOVERNOR"
+
+echo "Remember to run argoneone-install and argonone-config if you are using an Argon ONE case."
